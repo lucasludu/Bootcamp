a943d349d610f321acc3f613f1b5186c05c1a620
LucasLudu <lucaslud13@hotmail.com>
2022-05-08 22:12:13 -0300

    Se agregá 2 ejemplos de Async-Await en la clase 5

---
+++ Clase5/Async-Await/Ejemplo1/Ejemplo1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1
+{
+    public class Program
+    {
+        public static async Task process1()
+        {
+            await Task.Run(() =>
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Process 1");
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+
+        public static void process2()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Process 2");
+                Task.Delay(100).Wait();
+            }
+        }
+        static void Main(string[] args)
+        {
+            process1();
+            process2();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase5/Async-Await/Ejemplo2/Ejemplo2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo2
+{
+    internal class Program
+    {
+        public static void process1()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Process 1");
+                Task.Delay(100).Wait();
+            }
+        }
+
+        public static void process2()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Process 2");
+                Task.Delay(100).Wait();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            process1();
+            process2();
+            Console.ReadKey();
+        }
+    }
+}
