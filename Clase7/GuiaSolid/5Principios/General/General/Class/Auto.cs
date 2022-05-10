f90e6b4b0f5c5f7028d8676fd3fba9f162794518
LucasLudu <lucaslud13@hotmail.com>
2022-05-10 18:18:57 -0300

    Actualizo la clase 7

---
+++ Clase7/GuiaSolid/5Principios/General/General/Class/Auto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Class
+{
+    class Auto
+    {
+        private string modelo;
+        private int categoria;  // 0-Particular 1-Taxi 2-Carga
+        private 
+    }
+}
