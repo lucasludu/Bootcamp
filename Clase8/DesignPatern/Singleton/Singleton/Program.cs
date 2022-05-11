3f64a91b1aa4421860bb2fc1af2e0f41d502619e
LucasLudu <lucaslud13@hotmail.com>
2022-05-10 23:33:47 -0300

    Se agrega la clase 8 con carpeta de Patrones de diseño

---
+++ Clase8/DesignPatern/Singleton/Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // No se puede obtener la instancia directamente
+            // Singleton single = new Singleton();
+
+            // Obtenemos la instacia, se crea por primera vez
+            Singleton uno = Singleton.ObtenInstancia();
+
+            // Hacemos algo con la instancia
+            uno.PonerDatos("Ana", 27);
+            uno.AlgunProceso();
+
+            Console.WriteLine(uno);
+            Console.WriteLine("\n-------------\n");
+
+            // Obtenemos la instancia
+            Singleton dos = Singleton.ObtenInstancia();
+
+            // Comprobamos que es la misma instancia.
+            // Si lo es, tendra el mismo estado
+            Console.WriteLine(dos);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase8/DesignPatern/Singleton/Singleton/Singleton.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        // Aqui guardamos la unica instancia que va a existir
+        private static Singleton instancia;
+
+        // Datos propios de la clase
+        private string nombre;
+        private int edad;
+
+        // Se crea constructor privado
+        private Singleton()
+        {
+            nombre = "Sin asignar";
+            edad = 99;
+        }
+
+        public static Singleton ObtenInstancia()
+        {
+            // Se verifica si no existe la instancia
+            if(instancia == null)
+            {
+                // Si no existe, instanciamos
+                instancia = new Singleton();
+                Console.WriteLine("---- Instancia creada por primera vez----");
+            }
+            // Regresamos la instancia
+            return instancia;
+        }
+
+        // Aqui metodos propios de la clase
+        public override string ToString()
+        {
+            return String.Format("La persona {0}, tiene edad de {1}", nombre, edad);
+        }
+
+        public void PonerDatos(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        // Esto representa cualquier otro objeto
+        public void AlgunProceso()
+        {
+            Console.WriteLine("{0} esta trabajando en algo", nombre);
+        }
+    }
+}
