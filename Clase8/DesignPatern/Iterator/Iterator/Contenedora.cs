b17977fe40d94bc12719346a0aa0638d60a4b72f
LucasLudu <lucaslud13@hotmail.com>
2022-05-11 08:50:27 -0300

    Patron de diseño: Iterator

---
+++ Clase8/DesignPatern/Iterator/Iterator/Contenedora.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator 
+{
+    class Contenedora : IEnumerable
+    {
+        private int[] valores = new int[10];
+
+        public Contenedora()
+        {
+            for(int n = 0; n < 10; n++)
+            {
+                valores[n] = n * n;
+            }
+        }
+
+        // Se implementa GetEnumerator necesario por IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // Instanciamos el enumerador y lo regresamos
+            return (new ContenedorEnum(valores));
+        }
+    }
+
+    class ContenedorEnum : IEnumerator
+    {
+        public int[] arreglo;
+        private int posicion = -1;
+
+        public ContenedorEnum(int[] arreglo)
+        {
+            this.arreglo = arreglo;
+        }
+
+        public bool MoveNext()
+        {
+            posicion++;
+            if(posicion < arreglo.Length)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            posicion = -1;
+        }
+
+        public Object Current
+        {
+            get
+            {
+                return arreglo[posicion];
+            }
+        }
+
+    }
+
+}
---
+++ Clase8/DesignPatern/Iterator/Iterator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contenedora datos = new Contenedora();
+
+            foreach(int valor in datos)
+            {
+                Console.WriteLine(valor);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+ 
