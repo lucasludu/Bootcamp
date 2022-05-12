68db4e0d4fd7cfdcf5e47d4755bcaa898c543672
LucasLudu <lucaslud13@hotmail.com>
2022-05-12 20:58:41 -0300

    Ejercicio 1 singleton Clase 8

---
+++ Clase8/Ejercicio1/Ejercicio1/Country.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Country
+    {
+        private static Country _pais;
+
+        private string countryName;
+        private string[] countryProvinces;
+
+        public string CountryName { get => countryName; set => countryName = value; }
+        public string[] CountryProvinces { get => countryProvinces; set => countryProvinces = value; }
+
+        public Country(string countryName, string[] countryProvince)
+        {
+            this.countryName = countryName;
+            this.countryProvinces = countryProvinces;
+        }
+
+        public Country() 
+        {
+            countryName = "Sin asignar";
+            countryProvinces = null;
+        }
+
+    }
+}
